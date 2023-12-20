ccd924e07c6d11ed05a1a5dbb5a16dac4a05e7be
Albert Jannard <albertjannard@icloud.com>
2023-12-20 11:10:19 -0500

    fix img

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,7 @@ import Detail from "./pages/Detail";
 import Authentification from "./pages/Authentification";
 import { useState } from "react";
 
-// Importation des images
-import beignes from './assets/imgs/beignes.jpg';
-import boissonsGazeuse from './assets/imgs/boissonsGazeuse.jpg';
-import burgerPoulet from './assets/imgs/burgerPoulet.jpg';
-import salade from './assets/imgs/salade.jpg';
-import chips from './assets/imgs/chips.jpg';
-import sandwichJardin from './assets/imgs/sandwichJardin.jpg';
-import sandwichBucheron from './assets/imgs/sandwichBucheron.jpg';
-import coffe from './assets/imgs/coffe.jpg';
-import theGlace from './assets/imgs/theGlace.jpg';
+
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(false);
--- src/components/FicheItem.jsx
+++ src/components/FicheItem.jsx
@@ -1,5 +1,29 @@
 import { AiFillInfoCircle } from "react-icons/ai"
 import { Link } from 'react-router-dom';
+
+// Importation des images
+import beignes from '../assets/imgs/beignes.jpg';
+import boissonsGazeuse from '../assets/imgs/boissonsGazeuse.jpg';
+import burgerPoulet from '../assets/imgs/burgerPoulet.jpg';
+import salade from '../assets/imgs/salade.jpg';
+import chips from '../assets/imgs/chips.jpg';
+import sandwichJardin from '../assets/imgs/sandwichJardin.jpg';
+import sandwichBucheron from '../assets/imgs/sandwichBucheron.jpg';
+import coffe from '../assets/imgs/coffe.jpg';
+import theGlace from '../assets/imgs/theGlace.jpg';
+// Mapping des images
+const images = {
+  beignes: beignes,
+  boissonsGazeuse: boissonsGazeuse,
+  burgerPoulet: burgerPoulet,
+  salade: salade,
+  chips: chips,
+  sandwichJardin: sandwichJardin,
+  sandwichBucheron: sandwichBucheron,
+  coffe: coffe,
+  theGlace: theGlace,
+};
+
 const FicheItem = ({ item }) => {
   const maxLength = 150;
   const description = item.description.length > maxLength
@@ -21,13 +45,13 @@ const FicheItem = ({ item }) => {
       );
     }
   };
-  const imagePath = `/assets/imgs/${item.src}`
-
+  const image = images[item.src.split('.')[0]];
+  
 
   return (
     <div className="w-full px-3 relative flex flex-col justify-center text-zinc-900">
       <div className="relative flex right-3">
-        <img src={imagePath} alt={item.title} className="block w-40 h-36 relative left-8 -top-7 rounded-md shadow-md object-cover" />
+        <img src={image} alt={item.title} className="block w-40 h-36 relative left-8 -top-7 rounded-md shadow-md object-cover" />
         <div className='bg-gray-300 p-3 rounded-md min-w-min w-72 shadow-md pb-7'>
           <h3 className='pl-9 font-bold text-md w-full'>{item.title}</h3>
           <p className='pl-9 text-sm mt-2 w-full'>{description}</p> 
@@ -41,4 +65,6 @@ const FicheItem = ({ item }) => {
     </div>
   )
 }
+
+
 export default FicheItem; 
